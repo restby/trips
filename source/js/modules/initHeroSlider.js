05348752d13b3ad10c2f160c20c68fede9d80e69
restby <restby@gmail.com>
2023-09-15 07:52:21 +0300

    правки после тестирования v1.1

--- source/js/modules/initHeroSlider.js
+++ source/js/modules/initHeroSlider.js
@@ -1,34 +1,12 @@
 import Swiper from '../vendor/swiper';
+import {playAudio} from '../modules/audio-player';
+import {playVideo} from '../modules/video-player';
 
 const initHeroSlider = () => {
   if (!document.querySelector('[data-hero__slider]')) {
     return null;
   }
   const heroSlider = document.querySelector('[data-hero__slider]');
-
-  const pauseVideoAndReloadIframe = () => {
-    if (heroSlider.querySelector('iframe')) {
-
-      let iframe = heroSlider.querySelector('iframe');
-
-      const removeAutoplayParameter = (url) => {
-        const parts = url.split('?');
-        if (parts.length > 1) {
-          const params = parts[1].split('&');
-          const filteredParams = params.filter(function (param) {
-            return param !== 'autoplay=1';
-          });
-          return parts[0] + (filteredParams.length > 0 ? '?' + filteredParams.join('&') : '');
-        }
-        return url;
-      };
-
-      const videoUrl = iframe.src;
-      const cleanUrl = removeAutoplayParameter(videoUrl);
-
-      iframe.src = cleanUrl;
-    }
-  };
 
   return new Swiper(heroSlider, {
     simulateTouch: false,
@@ -37,8 +15,13 @@ const initHeroSlider = () => {
     autoHeight: true,
     on: {
       slideChange: () => {
-        if (heroSlider.querySelector('iframe')) {
-          pauseVideoAndReloadIframe();
+        if (document.querySelector('[data-id="audio"]').querySelector('iframe')) {
+          document.querySelector('[data-id="audio"]').querySelector('iframe').remove();
+          playAudio();
+        }
+        if (document.querySelector('[data-id="video"]').querySelector('iframe')) {
+          document.querySelector('[data-id="video"]').querySelector('iframe').remove();
+          playVideo();
         }
       },
     },
--- source/js/modules/video-player.js
+++ source/js/modules/video-player.js
@@ -1,18 +1,14 @@
 const playVideo = () => {
   if (document.querySelector('[data-id="video"]')) {
-    const video = document.querySelector('[data-id="video"]');
+    const videoPlayer = document.querySelector('[data-id="video"]');
     const videoBtnPlay = document.querySelector('.video__btn');
-    video.addEventListener('click', () => {
+    videoPlayer.addEventListener('click', () => {
 
-      if (video.hasAttribute('data-state')) {
-        return;
-      }
+      const iframe = `<iframe  id="youtube-video" src="${videoPlayer.dataset.src}" title="Бесплатные интерактивные онлайн-курсы." frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
 
-      video.dataset.state = 'ready';
-      const src = video.dataset.src;
-      video.insertAdjacentHTML('beforeend', '<iframe  id="youtube-video" src="' + src + '" title="Бесплатные интерактивные онлайн-курсы." frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>');
+      videoPlayer.insertAdjacentHTML('beforeend', iframe);
 
-      video.classList.toggle('video__bg-hide');
+      videoPlayer.classList.toggle('video__bg-hide');
       videoBtnPlay.classList.toggle('video__btn--hide');
     }, {once: true});
   }
